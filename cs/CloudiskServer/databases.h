7db6d2f835b9beee8c671726165465aaa956da4a
gold_waters <729301646@qq.com>
2024-06-20 15:29:30 +0800

    guo：第三期框架初级版

--- cs/CloudiskServer/databases.h
+++ cs/CloudiskServer/databases.h
@@ -9,12 +9,14 @@
 #include <mysql/mysql.h>
 
 typedef struct {
-    char parent_name[128];   // 父级目录名称，NULL 表示家目录
+    int file_id;
+    int parent_id;   // 父级目录名称，NULL 表示家目录
     char file_name[128];     // 要增加或删除的文件和目录
     int ower_id;
     char sha1[64];
     int filesize;
     char type;      // d 是目录， f是文件
+    char tomb;      // y 是活着， n是死
 } file_table;
 
 typedef struct {
@@ -22,21 +24,28 @@ typedef struct {
     char user_name[128];
     char crytpasswd[128];
     char pwd[128];
+    int pwd_id;
 } user_table;
 
 // (成功返回 0， 失败返回 -1)
-// 增加目录或上传文件
-int add_file_table(file_table* ptable);
+// 创建表
+int create_table(void);
 
-// 删除目录或文件
-int sub_file_table(file_table* ptable);
+// 增加目录或上传文件(虚拟文件表)
+int add_file_table(MYSQL* conn, file_table* ptable);
 
-// 要查找的目录或文件
-int select_file_table(file_table* ptable); 
+// 删除目录或文件(虚拟文件表)
+int sub_file_table(MYSQL* conn, file_table* ptable);
 
-// 增加用户
-int add_user_table(user_table* ptable);
+// 要查找的目录或文件(虚拟文件表)
+int select_file_table(MYSQL* conn, file_table* ptable); 
 
-// 查找用户
-int select_user_table(user_table* ptable);
+// 根据 SHA-1 HASH 查找所有文件表(全部文件表)
+int search_file(const char* sha1_hash);
+
+// 增加用户(用户表)
+int add_user_table(MYSQL* conn, user_table* ptable);
+
+// 查找用户(用户表)
+int select_user_table(MYSQL* conn, user_table* ptable);
 #endif
