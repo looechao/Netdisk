2cbe0c869114b7a4a42043ad44e0340b38a686fc
wangnan <724938542@qq.com>
2024-06-22 10:03:59 +0800

    refactor(client): split long and short commands

--- cs/CloudiskClient/main.c
+++ cs/CloudiskClient/main.c
@@ -1,17 +1,33 @@
 #include "client.h"
+#include "thread_pool.h"
 
-int main() {
+#define EPOLL_ARR_SIZE 100
+
+int main(void) {
     //连接服务器
-     int sockfd = my_connect("127.0.0.1", "8888");
+    int sockfd = my_connect("127.0.0.1", "8888");
                                                                   
-    //函数在login.c
+    //函数在client.c
     login(sockfd);
+
+    //创建线程池结构体
+    threadpool_t threadpool;
+    memset(&threadpool, 0, sizeof(threadpool));
+    // 初始化线程池
+    threadpoolInit(&threadpool, 1);
+    // 启动线程池
+    threadpoolStart(&threadpool);
+
+    // 创建epoll实例
+    //int epfd = epoll_create1(0);
+    //ERROR_CHECK(epfd, -1, "epoll_create1");
+    //struct epoll_event* pEventArr = (struct epoll_event*)calloc(EPOLL_ARR_SIZE, sizeof(struct epoll_event));
 
     char str[MAXLINE];
     for (; ; ) {
         printf(">> "); // 提示符
-        char * ret=fgets(str, MAXLINE, stdin); // 从stdin读取命令
-        if(ret==NULL){
+        char * ret = fgets(str, MAXLINE, stdin); // 从stdin读取命令
+        if(ret == NULL){
             printf("\n");
             break;    
         }
@@ -20,43 +36,55 @@ int main() {
             break; // 退出循环
         }
         
-        // 解析 并 发送命令
-        CmdType type= parse_input(str, sockfd);
+        // 解析命令  返回train_t
+        train_t t;
+        CmdType type = parse_input(str, &t);
         
+        // 长命令加入任务队列；短命令直接处理 
+        if (type == CMD_TYPE_GETS || type == CMD_TYPE_PUTS) {
+            task_t* ptask = (task_t*)calloc(1, sizeof(task_t));
+            ptask->t = t;
+            taskEnque(&threadpool.que, ptask);
+        }
+        else {
+            sendn(sockfd, &t, sizeof(t.len)+sizeof(t.type)+sizeof(t.file_size)+ t.len);
+        }
+
         // 接收服务器返回的信息 #所有人自己创建.c文件，如：gets.c文件存放 getsCommand(sockfd);   
-        switch(type){
+        switch (type) {
         case CMD_TYPE_PWD:  
-            //pwdCommand(sockfd);   
+            pwdCommand(sockfd);   
             break;
         case CMD_TYPE_CD:
-            //cdCommand(sockfd);    
+            cdCommand(sockfd);    
             break;
         case CMD_TYPE_LS:
-            //lsCommand(sockfd);    
+            lsCommand(sockfd);    
             break;
         case CMD_TYPE_MKDIR:
-            //mkdirCommand(sockfd);  
+            mkdirCommand(sockfd);  
             break;
         case CMD_TYPE_RMDIR:
-            //rmdirCommand(sockfd);  
+            rmdirCommand(sockfd);  
             break;
         case CMD_TYPE_RM:
-            //rmCommand(sockfd);  
+            rmCommand(sockfd);  
             break;
         case CMD_TYPE_NOTCMD:
-            //notCommand(sockfd);   
+            notCommand(sockfd);   
             break;
-        case CMD_TYPE_PUTS:
-            //putsCommand(sockfd);   
+       /* case CMD_TYPE_PUTS:
+            putsCommand(sockfd);   
             break;
         case CMD_TYPE_GETS:
-            //getsCommand(sockfd);   
-            break;
+            getsCommand(sockfd);   
+            break;*/
         default:
             break;
         }
     }
     close(sockfd);
+
     return 0;
 }
 
